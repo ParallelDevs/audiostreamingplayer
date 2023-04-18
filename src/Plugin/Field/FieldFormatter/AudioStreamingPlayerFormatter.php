76e609ba1220b31789e7bf86730abbbe31d8d6d9
jgaritapdevs <j.garita@paralleldevs.com>
2023-04-18 08:57:22 -0600

    Fix: coder errors.

--- src/Plugin/Field/FieldFormatter/AudioStreamingPlayerFormatter.php
+++ src/Plugin/Field/FieldFormatter/AudioStreamingPlayerFormatter.php
@@ -6,7 +6,6 @@ use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
 
-
 /**
  * Define the "audio streaming player formatter".
  *
@@ -19,78 +18,77 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
-
-
 class AudioStreamingPlayerFormatter extends FormatterBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return ['skin' => 'Theme '] + parent::defaultSettings();
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function defaultSettings() {
-        return [
-            'skin' => 'Theme ',
-        ] + parent::defaultSettings();
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form['skin'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Select the theme for the player'),
+      '#options' => [
+        0 => $this->t('Black Player'),
+        1 => $this->t('Circular Player'),
+        2 => $this->t('Only Text'),
+      ],
+      '#default_value' => $this->getSetting('skin'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    $about = '';
+
+    if ($this->getSetting('skin') == 0) {
+      $about = 'Black';
+    }
+    elseif ($this->getSetting('skin') == 1) {
+      $about = 'Circular';
+    }
+    elseif ($this->getSetting('skin') == 2) {
+      $about = 'Text';
+    }
+    $summary[] = $this->t("Chosen theme: @skin", ["@skin" => $about]);
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [];
+
+    if (($this->getSetting('skin')) == 0) {
+      $element[0] = [
+        '#theme' => 'audio_streaming_player_black',
+      ];
+    }
+    elseif (($this->getSetting('skin')) == 1) {
+      $element[0] = [
+        '#theme' => 'audio_streaming_player_circular',
+      ];
+    }
+    elseif (($this->getSetting('skin')) == 2) {
+      $element[0] = [
+        '#theme' => 'audio_streaming_player_text',
+      ];
     }
 
-    /**
-     * {@inheritdoc}
-     */
+    return $element;
+  }
 
-    public function settingsForm(array $form, FormStateInterface $form_state) {
-        $form['skin'] = [
-            '#type' => 'radios',
-            '#title' => $this->t('Select the theme for the player'),
-            '#options' => [
-                0 => $this->t('Black Player'),
-                1 => $this->t('Circular Player'),
-                2 => $this->t('Only Text'),
-
-            ],
-            '#default_value' => $this->getSetting('skin'),
-
-        ];
-        return $form;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function settingsSummary() {
-        $summary = [];
-        $about = '';
-
-        if ($this->getSetting('skin') == 0) {
-            $about = 'Black';
-        } else if ($this->getSetting('skin') == 1) {
-            $about = 'Circular';
-        } else if ($this->getSetting('skin') == 2) {
-            $about = 'Text';
-        }
-        $summary[] = $this->t("Chosen theme: @skin", ["@skin" => $about]);
-        return $summary;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-
-    public function viewElements(FieldItemListInterface $items, $langcode) {
-        $element = [];
-
-        if (($this->getSetting('skin')) == 0) {
-            $element[0] = [
-                '#theme' => 'audio_streaming_player_black',
-            ];
-        } else if (($this->getSetting('skin')) == 1) {
-            $element[0] = [
-                '#theme' => 'audio_streaming_player_circular',
-            ];
-        } else if (($this->getSetting('skin')) == 2) {
-            $element[0] = [
-                '#theme' => 'audio_streaming_player_text',
-            ];
-        }
-        return $element;
-    }
 }
